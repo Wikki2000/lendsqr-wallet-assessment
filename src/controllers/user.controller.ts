1d09b5a7de9081d86fe9de9594deae1d85c0ca5a
wikki2000 <wisdomokposin@gmail.com>
2025-08-02 17:04:33 +0100

    feat: add Karma blacklist check for user onboarding

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,7 +1,11 @@
 import { Request, Response } from 'express';
+import axios from 'axios';
 import { createUser } from '../services/user.service';
 import { isMissingFields } from '../utils/validate.utils';
 import { validatePasswordStrength } from '../utils/password.utils';
+
+const KARMA_API_URL = 'https://adjutor.lendsqr.com/v2/verification/karma/';
+const KARMA_API_KEY = process.env.KARMA_API_KEY!;
 
 export const createNewUser = async (req: Request, res: Response) => {
   try {
@@ -16,11 +20,41 @@ export const createNewUser = async (req: Request, res: Response) => {
     }
 
     const { email, firstName, phone, lastName, userName, password } = req.body;
+
+    // Check if user is in Lendsqr blacklist
+    try {
+      const karmaRes = await axios.get(`${KARMA_API_URL}${email}`, {
+        headers: {
+          Authorization: `Bearer ${KARMA_API_KEY}`
+        }
+      });
+
+      const kamData = karmaRes.data as {
+        data?: {
+          karma_type?: string;
+        };
+      };
+
+      /*
+      if (kamData.data?.karma_type) {
+        return res.status(403).json({
+          message: 'User is blacklisted in the Lendsqr Karma database and cannot be onboarded.',
+        });
+      }*/
+
+    } catch (error: unknown) {
+      const err = error as Error;
+      console.error('Karma API error:', error);
+      return res.status(502).json({ message: 'Failed to verify user with Karma API' });
+    }
+
+    // Validate password strength
     const strengthMsg: string | null = validatePasswordStrength(password);
     if (strengthMsg) {
       return res.status(422).json({ message: strengthMsg });
     }
 
+    // Create user
     const userId = await createUser({
       email, firstName, lastName, phone, userName, password
     });
@@ -29,10 +63,12 @@ export const createNewUser = async (req: Request, res: Response) => {
       message: 'User created successfully',
       user_id: userId,
     });
+
   } catch (error: any) {
     if (error.message === 'User already exists') {
       return res.status(409).json({ message: error.message });
     }
-    return res.status(500).json({ message: error.message });
+    return res.status(500).json({ message: error.message || 'Internal server error' });
   }
 };
+
